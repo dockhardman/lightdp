325425503d237b4f9deaa62d410a3760bb884bb6
Allen Chou <f1470891079@gmail.com>
2023-08-27 12:06:21 +0800

    New cli module.

---
+++ lightdp/cli/main.py
@@ -0,0 +1,23 @@
+import click
+
+from lightdp.version import VERSION
+
+
+@click.group()
+def app():
+    pass
+
+
+@click.command()
+@click.option("-vv", "-v", "verbose", is_flag=True, default=False, help="Verbose mode")
+@click.option("-s", "--short", "short", is_flag=True, default=False, help="Short mode")
+def version(verbose: bool, short: bool):
+    if short:
+        click.echo(VERSION)
+    else:
+        click.echo(f"lightdp version {VERSION}")
+        if verbose:
+            click.echo("Verbose mode enabled")
+
+
+app.add_command(version)
