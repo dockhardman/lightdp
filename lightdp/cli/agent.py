e9a92f71dd4f97130d43d1abb4c7118d018eaad2
Allen Chou <f1470891079@gmail.com>
2023-08-30 18:31:30 +0800

    Agent run cli

--- lightdp/cli/agent.py
+++ lightdp/cli/agent.py
@@ -1,6 +1,7 @@
 from typing import Text
 
 import click
+from rich import print
 
 from lightdp.agent import AgentPool
 from lightdp.broker import RedisBroker
@@ -13,21 +14,39 @@ def agent():
 
 @click.command()
 @click.option(
-    "--agent_name", "-a", required=False, default="AgentPool", help="Agent name"
+    "--name",
+    "-n",
+    required=False,
+    default="AgentPool",
+    envvar="AGENT_NAME",
+    help="Agent name",
 )
 @click.option(
     "--broker_url",
     "-b",
     required=False,
-    default="redis://localhost:30466/0",
+    default="redis://localhost:6379/0",
+    envvar="BROKER_URL",
     help="Broker URL",
 )
 @click.option(
-    "--channel_name", "-c", required=False, default="default", help="Channel name"
+    "--channel_name",
+    "--channel",
+    "-c",
+    required=False,
+    default="default",
+    envvar="CHANNEL_NAME",
+    help="Channel name",
 )
-def run(agent_name: Text, broker_url: Text, channel_name: Text):
-    agent = AgentPool(name=agent_name)
+def run(name: Text, broker_url: Text, channel_name: Text):
+    print(f"Agent name: '{name}'")
+    agent = AgentPool(name=name)
+
+    print(f"Broker URL: '{broker_url}'")
     broker = RedisBroker(url=broker_url)
+    broker.touch(channel_name=channel_name)
+    print(f"{broker.__class__.__name__} connected!")
+
     agent.run(broker=broker, channel_name=channel_name)
 
 
